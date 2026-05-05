2885a2c3b73b1ad189f1be0c54e2534286338364
Mayank Gupta <mayankguptasv01@gmail.com>
2026-05-05 23:14:17 +0530

    dashboard_navbar_updated

--- frontend/src/pages/page_components/Navbar.jsx
+++ frontend/src/pages/page_components/Navbar.jsx
@@ -29,23 +29,14 @@ export default function Navbar({
         </button>
 
         {/* Search */}
-        <div className="relative hidden sm:block">
-          <Search size={14} className="absolute left-3 top-1/2 -translate-y-1/2 text-neutral-300" />
-          <input
-            placeholder="Search courses, topics…"
-            className="border border-neutral-200 rounded-md pl-9 pr-3 h-8 text-sm outline-none focus:border-black transition bg-stone-50 w-56 placeholder:text-neutral-300"
-          />
-        </div>
+        
       </div>
 
       {/* Right Side */}
       <div className="flex items-center gap-3">
         
         {/* Notifications */}
-        <button className="relative text-neutral-400 hover:text-black transition">
-          <Bell size={16} />
-          <span className="absolute -top-0.5 -right-0.5 w-2 h-2 bg-blue-600 rounded-full" />
-        </button>
+        
 
         {/* User Avatar */}
         <Avatar className="w-7 h-7">
